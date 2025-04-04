a1becc40a4220e5d099918cb7ccea0a9b85d22c9
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-04-03 20:33:16 -0600

    feat: add display contoroller to create board

--- script.js
+++ script.js
@@ -1,85 +1,139 @@
-const GameBoard = (() => {
-  let board = Array(9).fill(null);
+const GameLogic = (() => {
 
-  const resetBoard = () => {
-    board = Array(9).fill(null);
-  };
+  const GameBoard = (() => {
+    let board = Array(9).fill(null);
 
-  const getBoardState = () => board;
+    const resetBoard = () => {
+      board = Array(9).fill(null);
+    };
 
-  const checkMoveValidity = (index, marker) => {
-    if (index >= 0 && index < 9 && board[index] === null) {
-      board[index] = marker;
-      return true;
-    }
-    return false;
-  };
+    const getBoardState = () => board;
 
-  return {getBoardState, checkMoveValidity, resetBoard};
-});
-
-const Player = (name, marker) => {
-  return { name, marker };
-};
-
-const GameController = (() => {
-  let players = [];
-  let activePlayer = 0;
-  let gameOver = false;
-
-  const newGame = () => {
-    players = [
-      Player('Player 1', 'X'),
-      Player('Player 2', 'O')
-    ];
-    activePlayer = 0;
-    gameOver = false;
-    Gameboard.reset();
-  };
-
-  const getActivePlayer = () => players[activePlayer];
-
-  const swapActivePlayer = () => {
-    activePlayer = activePlayer === 0 ? 1 : 0;
-  };
-
-  const checkWinCondition = () => {
-    const boardState = GameBoard.getBoardState();
-    const winConditions = [
-      [0,1,2], [3,4,5], [6,7,8],
-      [0,3,6], [1,4,7], [2,5,8],
-      [0,4,8], [2,4,6]
-    ];
-
-    for (const winCondition of winConditions) {
-      const [a,b,c] = winCondition;
-      if (board[a] && board[a] === board[b] && board[a] ===board[c]){
-        return board[a];
+    const checkMoveValidity = (index, marker) => {
+      if (index >= 0 && index < 9 && board[index] === null) {
+        board[index] = marker;
+        return true;
       }
-    }
+      return false;
+    };
 
-    if (!board.includes(null)) return 'draw';
-    return null;
+    return {getBoardState, checkMoveValidity, resetBoard};
+  });
+
+  const Player = (name, marker) => {
+    return { name, marker };
   };
 
-  const playerTurn = (index) => {
-    if (gameOver) return false;
+  const GameController = (() => {
+    let players = [];
+    let activePlayer = 0;
+    let gameOver = false;
 
-    const activePlayer = getActivePlayer();
+    const newGame = () => {
+      players = [
+        Player('Player 1', 'X'),
+        Player('Player 2', 'O')
+      ];
+      activePlayer = 0;
+      gameOver = false;
+      Gameboard.reset();
+    };
 
-    if (GameBoard.checkMoveValidity(index,activePlayer.marker)) {
-      const result = checkWinCondition();
+    const getActivePlayer = () => players[activePlayer];
 
-      if (result) {
-        gameOver = true;
-        return {gameOver: true, result};
+    const swapActivePlayer = () => {
+      activePlayer = activePlayer === 0 ? 1 : 0;
+    };
+
+    const checkWinCondition = () => {
+      const boardState = GameBoard.getBoardState();
+      const winConditions = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+      ];
+
+      for (const winCondition of winConditions) {
+        const [a,b,c] = winCondition;
+        if (board[a] && board[a] === board[b] && board[a] ===board[c]){
+          return board[a];
+        }
       }
-      swapActivePlayer();
-      return{gameOver:false, result:null};
-    }
-    return false;
-  };
-});
+
+      if (!board.includes(null)) return 'draw';
+      return null;
+    };
+
+    const playerTurn = (index) => {
+      if (gameOver) return false;
+
+      const activePlayer = getActivePlayer();
+
+      if (GameBoard.checkMoveValidity(index,activePlayer.marker)) {
+        const result = checkWinCondition();
+
+        if (result) {
+          gameOver = true;
+          return {gameOver: true, result};
+        }
+        swapActivePlayer();
+        return{gameOver:false, result:null};
+      }
+      return false;
+    };
+    const isGameOver = () => gameover;
+
+    return {newGame, getActivePlayer, playerTurn, isGameOver};
+  })();
+
+  const DisplayController = (() => {
+    const boardElement = document.getElementById('game-board');
+    const statusElement = document.getElementById('game-status');
+    const restartButton = document.getElementById('new-game-btn');    
+
+    const newGame = () => {
+      createBoard();
+      updateStatus(`Player X's turn.`);
+
+      restartButton.addEventListener('click', () => {
+        gameController.newGame();
+        updateBoard();
+        updateStatus(`Player X's turn.`);
+      });
+    };
+
+      createGameBoard = () => {
+        boardElement.innerHTML = '';
+
+        for (let i = 0; i < 9; i++) {
+          const cell = document.createElement('div');
+          cell.classList.add('cell');
+          cell.dataset.index = i;
+
+          cell.addEventListener('click', () => {
+            const result = gameController.playerTurn(i);
+            if (result) {
+              updateBard();
+
+              if (result.gameOver) {
+                if (result.result === 'draw') {
+                  updateStatus('Draw Game!');
+                  } else {
+                    updateStatus(`Player ${result.result} wins!`)
+                  }
+              } else {
+                const activePlayer = GameController.getActivePlayer();
+                updateStatus(`Player ${currentPlayer.marker}'s turn`);
+              }
+            }
+          });
+
+          boardElement.appendChild(cell);
+        }
+      };
+  });
+
+})();
 
 document.querySelector('#new-game-btn').addEventListener('click', () => {
   gameBoard();
